c79f3b38ea4bb33beb12acae08123e2b85e8a586
Rodrigo Araujo <dygufa@gmail.com>
2019-04-14 17:46:02 -0300

    update s3 key in order to fix deploy

--- aws-upload.conf.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    credentials:"aws-credentials.json",
-    bucketName:"adivinhe-site",
-    patterns:[
-        "index.html",
-        "favicon.ico",
-        "media/**/*"
-    ]
-}
